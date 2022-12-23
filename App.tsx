9294974abcdd8c9c05a725d61c1a6775fa58bee5
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-12-23 18:47:28 +0000

    app updated

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect } from "react"
+import { StatusBar } from "expo-status-bar"
+import * as SplashScreen from "expo-splash-screen"
 
+import { Home } from "./src/screens/Home"
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const SplashScreenHide = useCallback(async () => {
+    await SplashScreen.hideAsync()
+  }, [])
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreenHide()
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <StatusBar style="auto" hidden />
+      <Home />
+    </>
+  )
+}
